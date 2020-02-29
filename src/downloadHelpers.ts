f301fd7712656bdb3c2068de75dc8a4be8a76b57
Johan Gudmundsson <johan.gudmundsson@pickit.com>
2020-02-29 18:38:42 +0100

    Improve performance for downloading images

--- src/downloadHelpers.ts
+++ src/downloadHelpers.ts
@@ -1,32 +1,32 @@
 import fs from "fs";
 import request from "request";
-import { createDirIfNotExists } from "./fs-helpers";
+import { createDirIfNotExists, validFilename } from "./fs-helpers";
 
-export const downloadImage = async (uri: string, path: string, newFilename?: string): Promise<any> => {
+export const downloadImage = async (uri: string, savePath: string, newFilename?: string): Promise<any> => {
     return new Promise((resolve, reject) => {
-        if (!uri || !path) reject("Missing uri or path");
+        if (!uri || !savePath) return reject("Missing uri or path");
+        if (newFilename && !validFilename(newFilename)) return reject("Invalid filename");
+
+        let fullPath = '';
+        const splitedUri = uri.split("/");
+
+        if (newFilename) {
+            if (!newFilename.includes(".")) return reject("File extension missing");
+            fullPath = `${savePath}/${newFilename}`;
+        } else {
+            fullPath = `${savePath}/${splitedUri.pop()}`;
+        }
+
+        createDirIfNotExists(fullPath);
+
+        if (fs.existsSync(fullPath)) {
+            return reject("File already exists!");
+        }
         request.head(uri, (err: any) => {
             if (err) {
                 reject(err);
             }
-
-            let savePath = '';
-            const splitedUri = uri.split("/");
-
-            if (newFilename) {
-                if (!newFilename.includes(".")) reject("File extension missing");
-                savePath = `${path}/${newFilename}`;
-            } else {
-                savePath = `${path}/${splitedUri.pop()}`;
-            }
-
-            createDirIfNotExists(path);
-
-            if (fs.existsSync(savePath)) {
-                reject("File already exists!");
-            }
-
-            const stream = request(uri).pipe(fs.createWriteStream(savePath));
+            const stream = request(uri).pipe(fs.createWriteStream(fullPath));
             stream.on('close', resolve);
             stream.on('error', (e: any) => { reject(e) })
         });
