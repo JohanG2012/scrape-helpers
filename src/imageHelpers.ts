54bae3debbb2e6bff48cd3e4b259ab9d9b310630
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2020-02-29 21:21:12 +0100

    Add resize image method

--- src/imageHelpers.ts
+++ src/imageHelpers.ts
@@ -1,16 +1,27 @@
 import gm from "gm";
 
+export const getImageSize = (file: string): Promise<any> =>
+  new Promise((resolve, reject) => {
+    gm(file).size(function(err, size) {
+      if (err) return reject(err);
+      if (!size) return reject("Could not read image size");
+      return resolve({
+        width: size.width,
+        height: size.height
+      });
+    });
+  });
 
-
-export const getImageSize = (file: string): Promise<any> => new Promise((resolve, reject) => {
-    gm(file)
-        .size(function (err, size) {
-            console.log({ err, size })
-            if (err) return reject(err);
-            if (!size) return reject("Could not read image size");
-            return resolve({
-                width: size.width,
-                height: size.height
-            })
-        });
-})
+export const resizeImage = (
+  path: string,
+  width: number,
+  height: number
+): Promise<any> =>
+  new Promise((resolve, reject) => {
+    gm(path)
+      .resize(width, height)
+      .write(path, function(err) {
+        if (err) return reject(err);
+        return resolve();
+      });
+  });
