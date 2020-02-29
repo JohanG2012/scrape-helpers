4ea962a7916f561a69ba1e15789ca300ed7b9b06
Johan Gudmundsson <johan.gudmundsson2012@gmail.com>
2020-02-29 21:20:48 +0100

    Fix bug with folder being created instead of file

--- src/downloadHelpers.ts
+++ src/downloadHelpers.ts
@@ -2,33 +2,40 @@ import fs from "fs";
 import request from "request";
 import { createDirIfNotExists, validFilename } from "./fs-helpers";
 
-export const downloadImage = async (uri: string, savePath: string, newFilename?: string): Promise<any> => {
-    return new Promise((resolve, reject) => {
-        if (!uri || !savePath) return reject("Missing uri or path");
-        if (newFilename && !validFilename(newFilename)) return reject("Invalid filename");
+export const downloadImage = async (
+  uri: string,
+  savePath: string,
+  newFilename?: string
+): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    if (!uri || !savePath) return reject("Missing uri or path");
+    if (newFilename && !validFilename(newFilename))
+      return reject("Invalid filename");
 
-        let fullPath = '';
-        const splitedUri = uri.split("/");
+    let fullPath = "";
+    const splitedUri = uri.split("/");
 
-        if (newFilename) {
-            if (!newFilename.includes(".")) return reject("File extension missing");
-            fullPath = `${savePath}/${newFilename}`;
-        } else {
-            fullPath = `${savePath}/${splitedUri.pop()}`;
-        }
+    if (newFilename) {
+      if (!newFilename.includes(".")) return reject("File extension missing");
+      fullPath = `${savePath}/${newFilename}`;
+    } else {
+      fullPath = `${savePath}/${splitedUri.pop()}`;
+    }
 
-        createDirIfNotExists(fullPath);
+    createDirIfNotExists(savePath);
 
-        if (fs.existsSync(fullPath)) {
-            return reject("File already exists!");
-        }
-        request.head(uri, (err: any) => {
-            if (err) {
-                reject(err);
-            }
-            const stream = request(uri).pipe(fs.createWriteStream(fullPath));
-            stream.on('close', resolve);
-            stream.on('error', (e: any) => { reject(e) })
-        });
+    if (fs.existsSync(fullPath)) {
+      return reject("File already exists!");
+    }
+    request.head(uri, (err: any) => {
+      if (err) {
+        reject(err);
+      }
+      const stream = request(uri).pipe(fs.createWriteStream(fullPath));
+      stream.on("close", resolve);
+      stream.on("error", (e: any) => {
+        reject(e);
+      });
     });
-}
+  });
+};
